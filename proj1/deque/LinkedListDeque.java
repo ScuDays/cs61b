477dc92e45c3ce6fa3f77cad7e97c59eb0187638
TWorstPlayer <1339653485@qq.com>
2023-08-09 21:03:13 +0800

    improve project1

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,3 +1,4 @@
+/*
 package deque;
 
 public class LinkedListDeque<T> {
@@ -101,3 +102,134 @@ public class LinkedListDeque<T> {
    }
 
 }
+*/
+
+
+
+package deque;
+
+public class LinkedListDeque<T> {
+
+    private IntNode sentinel;
+    private IntNode sentinelLast;
+    private int cache;
+
+    public class IntNode{
+        IntNode last;
+        T own;
+        IntNode next;
+
+        public IntNode(){
+            this.last=null;
+            this.own=null;
+            this.next=null;
+        }
+        public IntNode(T own){
+            this.last=null;
+            this.next=null;
+            this.own=own;
+        }
+    }
+
+
+
+    public  LinkedListDeque(){
+        sentinel =new IntNode();
+        sentinelLast =new IntNode();
+
+        sentinel.next=sentinelLast;
+        sentinelLast.last=sentinel;
+
+        this.cache=0;
+
+    }
+
+    public void addFirst(T i) {
+        IntNode a=new IntNode();
+        a.own=i;
+        if(cache==0){
+            sentinel.next=a;
+            a.last=sentinel;
+            a.next=sentinel;
+            sentinelLast.last=a;
+        }
+        else{
+            IntNode b=sentinel.next;
+            sentinel.next=a;
+            a.last=sentinel;
+            a.next=b;
+            b.last=a;
+        }
+
+        cache++;
+
+    }
+
+    public T removeFirst() {
+        IntNode a;
+        if (cache!=0) {
+            a = sentinel.next.next;
+            T Return =sentinel.next.own;
+            sentinel.next=a;
+            a.last=sentinel;
+            cache--;
+            return  Return;
+        }
+        return null;
+
+
+    }
+
+    public boolean isEmpty() {
+        if(cache==0)return true;
+        else return false;
+    }
+
+    public void addLast(T i) {
+        IntNode add=new IntNode();
+        add.own=i;
+        sentinelLast.last.next=add;
+        add.last=sentinelLast.last;
+        add.next=sentinelLast;
+        sentinelLast.last=add;
+
+        cache++;
+
+    }
+
+    public T removeLast() {
+        if(cache==0)return null;
+
+        IntNode finalNode =sentinelLast.last;
+        T Return =finalNode.own;
+        sentinelLast.last=sentinelLast.last.last;
+
+        sentinelLast.last.next=sentinelLast;
+        cache--;
+        return Return;
+
+    }
+
+    public int size() {
+        return cache;
+    }
+
+    public void printDeque() {
+
+        if(cache==0){
+            return;
+        }
+        IntNode finalNode=sentinel;
+        int cache1=cache;
+        while(cache1!=0){
+            finalNode=finalNode.next;
+            System.out.println(finalNode.own);
+            cache1--;
+        }
+    }
+
+
+
+
+}
+
--- proj1/deque/LinkedListDequeTest.java
+++ proj1/deque/LinkedListDequeTest.java
@@ -17,22 +17,22 @@ import static org.junit.Assert.*;
         //System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
         LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
-		assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
-		lld1.addFirst("front");
+        assertTrue("A newly initialized LLDeque should be empty", lld1.isEmpty());
+        lld1.addFirst("front");
 
-		// The && operator is the same as "and" in Python.
-		// It's a binary operator that returns true if both arguments true, and false otherwise.
+        // The && operator is the same as "and" in Python.
+        // It's a binary operator that returns true if both arguments true, and false otherwise.
         assertEquals(1, lld1.size());
         assertFalse("lld1 should now contain 1 item", lld1.isEmpty());
 
-		lld1.addLast("middle");
-		assertEquals(2, lld1.size());
+        lld1.addLast("middle");
+        assertEquals(2, lld1.size());
 
-		lld1.addLast("back");
-		assertEquals(3, lld1.size());
+        lld1.addLast("back");
+        assertEquals(3, lld1.size());
 
-		System.out.println("Printing out deque: ");
-		lld1.printDeque();
+        System.out.println("Printing out deque: ");
+        lld1.printDeque();
     }
 
     //测试通过
@@ -42,16 +42,16 @@ import static org.junit.Assert.*;
 
       //  System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
         LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
-		// should be empty
-		assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
+        // should be empty
+        assertTrue("lld1 should be empty upon initialization", lld1.isEmpty());
 
-		lld1.addFirst(10);
-		// should not be empty
-		assertFalse("lld1 should contain 1 item", lld1.isEmpty());
+        lld1.addFirst(10);
+        // should not be empty
+        assertFalse("lld1 should contain 1 item", lld1.isEmpty());
 
-		lld1.removeFirst();
-		// should be empty
-		assertTrue("lld1 should be empty after removal", lld1.isEmpty());
+        lld1.removeFirst();
+        // should be empty
+        assertTrue("lld1 should be empty after removal", lld1.isEmpty());
     }
 
     //测试通过
@@ -119,9 +119,11 @@ import static org.junit.Assert.*;
             lld1.addLast(i);
         }
 
+
         for (double i = 0; i < 500000; i++) {
             assertEquals("Should have the same value", i, (double) lld1.removeFirst(), 0.0);
         }
+
 
         for (double i = 999999; i > 500000; i--) {
             assertEquals("Should have the same value", i, (double) lld1.removeLast(), 0.0);
