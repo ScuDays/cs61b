34d7b1fb554efdc198a309fd694c62f5204199f8
ScuDays <dayss1224@gmail.com>
2024-03-10 20:15:44 +0800

    merge的两种特殊情况判断

--- proj2/gitlet/MergeMethod.java
+++ proj2/gitlet/MergeMethod.java
@@ -5,7 +5,6 @@ import java.util.*;
 public class MergeMethod {
     public static void Merge(String theBranchName) {
         /** 首先得找到最近的共同分歧Commit*/
-
         /** 建立一个Map，把要merge的分支加到的所有Commit加到Map里面，然后从当前分支开始一个一个往前，和Map对比，看是否在里面*/
         Set<String> BranchCommitMap = new TreeSet<>();
         BranchPointer theBranch = BranchPointer.ReadBranchPointer(theBranchName);
@@ -35,7 +34,7 @@ public class MergeMethod {
         BranchPointer addBranch = BranchPointer.ReadBranchPointer(theBranchName);
         ArrayList<BranchPointer.Node> AddNodeList = addBranch.NodeList;
 
-        for (int i = 0; i < AddNodeList.size(); i++){
+        for (int i = 0; i < AddNodeList.size(); i++) {
             BranchCommitMap.add(AddNodeList.get(i).CommitSha1Name);
             if (AddNodeList.get(i).CommitSha1Name.equals(divideCommitName)) {
                 addBranchName = addBranch.parentBranch;
@@ -71,8 +70,22 @@ public class MergeMethod {
             }
             if (ok == true) break;
         }
-        System.out.println(SplitCommitName);
-        Log.global_log();
-        System.exit(0);
+
+        /** 找到splitCommit 先进行错误判断*/
+
+        /** 情况一：other分支的最新commit就是SplitCommit ——> 说明other分支就在当前分支上，只不过落后于当前分支
+         *  相当于已经merge了，输出Given branch is an ancestor of the current branch.即可*/
+        if (SplitCommitName.equals(theBranch.getCurrentLocation())) {
+            System.out.println("Given branch is an ancestor of the current branch.");
+            System.exit(0);
+        }
+        /** 情况二：当前分支的最新commit就是SplitCommit ——>说明当前分支落后于other分支，other分支就是从当前最新commit分支出去的
+         *  将head设为other分支的最新commit，同时打印 Given branch is an ancestor of the current branch.即可*/
+        if(head.getCurrentLocation().equals(SplitCommitName)){
+            System.out.println("Current branch fast-forwarded.");
+            head.setCurrentLocation(theBranch.getCurrentLocation());
+            head.setPointer_Name(theBranchName);
+            head.SerializeStore();
+        }
     }
 }
